bd06d87537cb3b66c76e0ab70083cfb488ff482e
Leyan05 <148929020+Leyan05@users.noreply.github.com>
2025-11-19 16:03:03 -0600

    Add files via upload

--- television.py
+++ television.py
@@ -9,55 +9,57 @@ class Television:
         Initial settings:
         Power is off, volume is at minimum, channel is at minimum, and mute is off.
         """
-        self.status = False  # TV is initially off
-        self.muted = False  # Mute is initially off
-        self.volume = Television.MIN_VOLUME  # Initial volume
-        self.channel = Television.MIN_CHANNEL  # Initial channel
+        self.__status = False  # TV is initially off
+        self.__muted = False  # Mute is initially off
+        self.__volume = Television.MIN_VOLUME  # Initial volume
+        self.__channel = Television.MIN_CHANNEL  # Initial channel
     
     def power(self) -> None:
-        """Toggle the power status of the television."""
-        self.status = not self.status
+        """Toggle the power status of the television.
+        :return: None"""
+        self.__status = not self.__status
     
     def mute(self) -> None:
-        """Toggle the mute status of the television if the power is on."""
-        if self.status:
-            self.muted = not self.muted
+        """Toggle the mute status of the television if the power is on.
+        :return: None"""
+        if self.__status:
+            self.__muted = not self.__muted
     
     def channel_up(self) -> None:
-        """Increase the channel number by one, wrapping to the minimum channel if at the maximum."""
-        if self.status:
-            if self.channel < Television.MAX_CHANNEL:
-                self.channel += 1
+        """Increase the channel number by one, wrapping to the minimum channel if at the maximum.
+        :return: None"""
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
             else:
-                self.channel = Television.MIN_CHANNEL
+                self.__channel = Television.MIN_CHANNEL
     
     def channel_down(self) -> None:
-        """Decrease the channel number by one, wrapping to the maximum channel if at the minimum."""
-        if self.status:
-            if self.channel > Television.MIN_CHANNEL:
-                self.channel -= 1
+        """Decrease the channel number by one, wrapping to the maximum channel if at the minimum.
+        :return: None"""
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
             else:
-                self.channel = Television.MAX_CHANNEL
+                self.__channel = Television.MAX_CHANNEL
     
     def volume_up(self) -> None:
         """Increase the volume by one, up to the maximum volume, while also
-        unmuting the TV if it was muted.
-        """
-        if self.status:
-            self.muted = False  # Unmute when volume is adjusted
-            if self.volume < Television.MAX_VOLUME:
-                self.volume += 1
+        unmuting the TV if it was muted. :return: None"""
+        if self.__status:
+            self.__muted = False  # Unmute when volume is adjusted
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
 
     def volume_down(self) -> None:
         """Decrease the volume by one, down to the minimum volume, while also
-        unmuting the TV if it was muted.
-        """
-        if self.status:
-            self.muted = False  # Unmute when volume is adjusted
-            if self.volume > Television.MIN_VOLUME:
-                self.volume -= 1
+        unmuting the TV if it was muted. :return: None"""
+        if self.__status:
+            self.__muted = False  # Unmute when volume is adjusted
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
 
     def __str__(self) -> str:
-        """Return the string representation of the television's current state."""
-        volume_display = 0 if self.muted else self.volume
-        return f"Power = {self.status}, Channel = {self.channel}, Volume = {volume_display}"
+        """Return the string representation of the television's current state. :return: str"""
+        volume_display = 0 if self.__muted else self.__volume
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {volume_display}"
